d2c5e8f5068b6d8c8dff714c765d350dacd857f3
Mike Sperber <sperber@deinprogramm.de>
2022-03-11 17:05:56 +0100

    Update.

--- functional-kotlin/src/main/kotlin/List.kt
+++ functional-kotlin/src/main/kotlin/List.kt
@@ -1,9 +1,15 @@
 // A list is one the following:
 // - the empty list - OR -
 // - a cons list consisting of the first element and a rest list
-sealed interface List<A>
+sealed interface List<out A> // variance annotation
 object Empty : List<Nothing>
 data class Cons<A>(val first: A, val rest: List<A>): List<A>
 
 val list1 = Cons(5, Empty)
 val list2 = Cons ("Mike", Cons("Sperber", Empty))
+
+fun listSum(list: List<Int>): Int =
+    when (list) {
+        is Empty -> 0
+        is Cons -> list.first + listSum(list.rest)
+    }
