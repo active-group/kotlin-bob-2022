d120ad062f8573fc976f8af386c2957c225c28ea
Mike Sperber <sperber@deinprogramm.de>
2022-03-11 17:02:02 +0100

    Update.

--- functional-kotlin/src/main/kotlin/List.kt
+++ functional-kotlin/src/main/kotlin/List.kt
@@ -2,4 +2,8 @@
 // - the empty list - OR -
 // - a cons list consisting of the first element and a rest list
 sealed interface List<A>
-object Empty : List<A>
+object Empty : List<Nothing>
+data class Cons<A>(val first: A, val rest: List<A>): List<A>
+
+val list1 = Cons(5, Empty)
+val list2 = Cons ("Mike", Cons("Sperber", Empty))
