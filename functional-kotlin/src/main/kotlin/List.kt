f31098e38ffbc02a3aed1d064ccfbb19dc1383d4
Mike Sperber <sperber@deinprogramm.de>
2022-03-11 17:00:03 +0100

    Update.

--- functional-kotlin/src/main/kotlin/List.kt
+++ functional-kotlin/src/main/kotlin/List.kt
@@ -1,2 +1,5 @@
-class List {
-}
+// A list is one the following:
+// - the empty list - OR -
+// - a cons list consisting of the first element and a rest list
+sealed interface List<A>
+object Empty : List<A>
